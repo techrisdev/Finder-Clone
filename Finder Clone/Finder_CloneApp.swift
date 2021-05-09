2300f005d71efd07c1c226cfc85e6ecea990fc7c
techrisdev <techrisdev>
2021-05-09 16:52:55 +0200

    Initial Commit

---
+++ Finder Clone/Finder_CloneApp.swift
@@ -0,0 +1,14 @@
+// Finder_CloneApp.swift
+//
+// Created by TeChris on 09.05.21.
+
+import SwiftUI
+
+@main
+struct Finder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
